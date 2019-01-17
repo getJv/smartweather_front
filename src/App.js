3f3b13dc2534601a4323f7af286194de0353a76f
Jhonatan Morais <jhonatanvinicius@gmail.com>
2019-01-17 20:50:05 -0200

    refactoring...

--- src/App.js
+++ src/App.js
@@ -1,88 +1,52 @@
 import React, { Component } from 'react';
-import $ from 'jquery';
 import './css/pure-min.css';
 import './css/side-menu.css';
+import SearchFormComponent from './components/SearchFormComponent';
 
-import SearchInputComponent from './components/SearchInputComponent';
 
 
 class App extends Component {
-  
-  constructor(){
-    super();
-    this.state = {querySearch:'',location:{}};
-    this.sentForm = this.sentForm.bind(this)
-    this.setQueryString = this.setQueryString.bind(this)
-  }
 
-  componentWillMount(){
-   
-  }
-  componentDidMount(){
-    $.ajax({
-      url: "http://localhost:8080/weather/brasilia",
-      dataType:'json',
-        success: ans => this.setState({wheatherData:ans}),
-        error: err => console.log(err) 
-    });
-  }
 
-  sentForm(event){
-      event.preventDefault();
-      //setState(this.state.querySearch) = '';
-    console.log("dados sendo enviados");
-  }
+    render() {
+        return (
+            <div id="layout">
 
-  setQueryString(event){
-        this.setState({querySearch:event.target.value})
-  }
+                <a href="#menu" id="menuLink" className="menu-link">
 
-  
-  
-  render() {
-    return (
-<div id="layout">
-    
-    <a href="#menu" id="menuLink" className="menu-link">
-       
-        <span></span>
-    </a>
+                    <span></span>
+                </a>
 
-    <div id="menu">
-        <div className="pure-menu">
-            <a className="pure-menu-heading" href="#">Menu</a>
+                <div id="menu">
+                    <div className="pure-menu">
+                        <a className="pure-menu-heading" href="http://teste">Menu</a>
 
-            <ul className="pure-menu-list">
-                <li className="pure-menu-item menu-item-divided pure-menu-selected">
-                    <a href="#" className="pure-menu-link">Home</a>
-                </li>
+                        <ul className="pure-menu-list">
+                            <li className="pure-menu-item menu-item-divided pure-menu-selected">
+                                <a href="http://teste" className="pure-menu-link">Home</a>
+                            </li>
 
-                <li className="pure-menu-item"><a href="#" className="pure-menu-link">About</a></li>
-            </ul>
-        </div>
-    </div>
+                            <li className="pure-menu-item"><a href="http://teste" className="pure-menu-link">About</a></li>
+                        </ul>
+                    </div>
+                </div>
 
-    <div id="main">
-        <div className="header">
-            <h1>Smart Wheather</h1>
-            <h2>The better option to follow the wheather in your region</h2>
-        </div>
+                <div id="main">
+                    <div className="header">
+                        <h1>Smart Wheather</h1>
+                        <h2>The better option to follow the wheather in your region</h2>
+                    </div>
 
-        <div className="content">
-              <br/>
-              <div className="pure-form pure-form-aligned">
-                <form className="pure-form pure-form-aligned" onSubmit={this.sentForm} method="post">
-                <SearchInputComponent id="location" placeholder="What is your location?" type="text" name="location" value={this.state.querySearch} onChange={this.setQueryString}  />  
-                </form>        
+                    <div className="content">
+                        <br />
+                        <SearchFormComponent />
+                        
+                    </div>
+                </div>
+            </div>
 
-               searching for: {this.state.querySearch}
-              </div>  
-        </div>
-    </div>
-</div>
-
-    );
-  }
+        );
+    }
 }
 
 export default App;
---
+++ src/components/SearchInputComponent.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+export default class SearchInputComponent extends Component{
+
+    render() {
+        return (
+            <div className="pure-control-group">
+                <input
+                    id={this.props.id}
+                    placeholder={this.props.placeholder}
+                    type={this.props.type}
+                    name={this.props.name}
+                    value={this.props.value}
+                    onChange={this.props.onChange}
+                />
+            </div>
+        );
+    }
+
+
+}
