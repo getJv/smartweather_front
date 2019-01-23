17388ad21597315bbb1cf0ded832269e5d24597f
Jhonatan Morais <jhonatanvinicius@gmail.com>
2019-01-23 13:56:30 -0200

    location finder it is ok

--- src/components/MapComponent.js
+++ src/components/MapComponent.js
@@ -4,37 +4,30 @@ import PubSub from 'pubsub-js';
 
 import Map from 'pigeon-maps'
 import Marker from 'pigeon-marker'
-import Overlay from 'pigeon-overlay'
 
 class MapComponent extends Component {
 
-    constructor(props) {
-        super(props)
-
-        this.state = {
-            center: [50.879, 4.6997],
-            zoom: 12
-        }
-    }
+   
 
     handleMarkerClick = ({ event, payload, anchor }) => {
         console.log(`Marker #${payload} clicked at: `, anchor)
     }
 
     render() {
-        const { center, zoom } = this.state
+       
 
 
         return (
             <div className="pure-g">
                 <div className="pure-u-1" >
-                    <Map center={center}
-                        zoom={zoom}
+                    <Map center={this.props.points}
+                        zoom={12}
                         height={150}>
-                        <Marker anchor={[50.879, 4.6997]} payload={1} onClick={this.handleMarkerClick} />
+                        <Marker anchor={this.props.points} payload={1} onClick={this.handleMarkerClick} />
                     </Map>
-                    {console.log(this.props.locationPoints.location)}
-                    {this.props.locationPoints.location.name}
+                
+                    {}
+                    
                 </div>
             </div >
         );
@@ -47,22 +40,25 @@ export default class WeatherBox extends Component {
 
     constructor() {
         super();
-        this.state = { wheatherData: {} };
+        this.state = { points:[50.879, 4.6997], wheatherData: {} };
 
     }
 
-    componentDidMount() {
+    componentWillMount() {
         PubSub.subscribe('newWeatherDataStream', (stream, data) => {
-            this.setState({ wheatherData: data });
+            
+            if(data.location)
+            this.setState({ points: [data.location.lat, data.location.lon], wheatherData: data });
 
         });
+        
     }
 
     render() {
         return (
             <div > 
                 <SearchFormComponent />
-                <MapComponent locationPoints={this.state.wheatherData} />
+                <MapComponent points={this.state.points}  />
                 
             </div>
         );
