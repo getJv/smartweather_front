4dd5d324434abbce0c66bb2f86d6a7c1823229c7
Jhonatan Morais <jhonatanvinicius@gmail.com>
2019-01-25 11:11:19 -0200

    build files

---
+++ build/precache-manifest.6e258363132e3f08f43e0ac37ec24be9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "79b1d9593a3e7976fb54",
+    "url": "/static/js/main.79b1d959.chunk.js"
+  },
+  {
+    "revision": "b6ce03270c1e92ebe460",
+    "url": "/static/js/1.b6ce0327.chunk.js"
+  },
+  {
+    "revision": "79b1d9593a3e7976fb54",
+    "url": "/static/css/main.f0cf0fb1.chunk.css"
+  },
+  {
+    "revision": "452e2395c4d9fc4ce616a3def27f0176",
+    "url": "/index.html"
+  }
+];
